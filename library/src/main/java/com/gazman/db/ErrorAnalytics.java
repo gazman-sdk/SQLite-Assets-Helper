4163409c5b2e4874d16f85492ff92d9ac498b4c1
Ilya Gazman <gazman1986@gmail.com>
2019-06-14 10:01:19 -0400

    Added Crashlytics local logs

--- library/src/main/java/com/gazman/db/ErrorAnalytics.java
+++ library/src/main/java/com/gazman/db/ErrorAnalytics.java
@@ -4,6 +4,9 @@ import com.crashlytics.android.Crashlytics;
 
 class ErrorAnalytics {
     public static void logException(Exception e) {
+        if(BuildConfig.DEBUG) {
+            e.printStackTrace();
+        }
         Crashlytics.logException(e);
     }
 }
