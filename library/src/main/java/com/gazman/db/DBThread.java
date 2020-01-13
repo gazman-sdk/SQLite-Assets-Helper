d4e6a609a13360a6e2b46d53654efaee2cd20508
Ilya Gazman <gazman1986@gmail.com>
2020-01-12 20:08:26 -0500

    Added logs

--- library/src/main/java/com/gazman/db/DBThread.java
+++ library/src/main/java/com/gazman/db/DBThread.java
@@ -26,10 +26,30 @@ public final class DBThread {
             Thread.yield();
         }
 
-        if (handler.getLooper() == Looper.myLooper()) {
+        if (isOnDbThread()) {
             runnable.run();
         } else {
             handler.post(runnable);
         }
+    }
+
+    public static void synchronize() {
+        synchronized (DBThread.class){
+            execute(() -> {
+                synchronized (DBThread.class){
+                    DBThread.class.notifyAll();
+                }
+            });
+            try {
+                DBThread.class.wait();
+            } catch (InterruptedException e) {
+                ErrorAnalytics.logException(e);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static boolean isOnDbThread() {
+        return handler.getLooper() == Looper.myLooper();
     }
 }
